d3dffde3e33de644b3b3332d51abbcadf38e5429
Pierce <Pierce@PIERCE>
2024-09-27 14:23:11 -0400

    change baseURL

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import * as MODEL from "../model/model.js";
 
 // Variables
 export let APIkey = "ec6efbb25da2451c8fe152440242309";
-export let baseURL = "http://api.weatherapi.com/v1";
+export let baseURL = "https://api.weatherapi.com/v1";
 
 // Functions
 function initListeners() {
